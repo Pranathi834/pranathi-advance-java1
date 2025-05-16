359e08e7c9909dcc543c0210c8b8ebc6ec07b500
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-16 18:28:33 +0530

    Add files via upload

---
+++ program1C2.java
@@ -0,0 +1,32 @@
+package program1C2;
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class program1C2 {
+	
+	    public static void main(String[] args) {
+	        // Create a LinkedList of Strings
+	        LinkedList<String> colors = new LinkedList<>();
+
+	        // Add elements to the LinkedList
+	        colors.add("Red");
+	        colors.add("Green");
+	        colors.add("Blue");
+	        colors.add("Yellow");
+	        colors.add("Purple");
+
+	        // Display the original LinkedList
+	        System.out.println("Original LinkedList: " + colors);
+
+	        // Create a descending iterator (reverse order)
+	        Iterator<String> reverseIterator = colors.descendingIterator();
+
+	        // Iterate in reverse order
+	        System.out.println("Iterating in reverse order:");
+	        while (reverseIterator.hasNext()) {
+	            System.out.println(reverseIterator.next());
+	        }
+	    }
+	}
+
+
