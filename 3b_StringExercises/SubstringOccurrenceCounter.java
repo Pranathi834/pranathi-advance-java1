ad0e21bb892f2665b24e47d72570d4076e1d9da8
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-19 20:28:51 +0530

    move 3b_StringExercises to SubstringOccurrenceCounter.java

--- 3b_StringExercises/SubstringOccurrenceCounter.java
+++ 3b_StringExercises/SubstringOccurrenceCounter.java
@@ -1,6 +1,8 @@
+*/Q2. Write a Java Program for Counting how many times a substring appears in a main string
+using user defined function countOccurrences() */
 package program3B2;
 
-public class program3B2 {
+public class SubstringOccurrenceCounter {
 	    public static int countOccurrences(String mainStr, String subStr) {
 	        int count = 0;
 	        int index = 0;
