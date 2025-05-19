38cfebe80dffbfb3d88c9054da16ff9ef97961a1
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-19 11:10:18 +0530

    Update and rename progrm1B4.java to ColorSubListDemo.java

--- ColorSubListDemo.java
+++ ColorSubListDemo.java
@@ -1,7 +1,9 @@
+/*1B4 Write a java program for getting different colors through ArrayList interface and extract the
+elements 1st and 2nd from the ArrayList object by using SubList()*/
 package progrm1B4;
 import java.util.ArrayList;
 import java.util.List;
-public class progrm1B4 {
+public class ColorSubListDemo {
 	
 	    public static void main(String[] args) {
 	        // Create an ArrayList to store colors
