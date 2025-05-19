804aabb84e0ef748b1bb4f946679131b38678ebf
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-19 20:56:41 +0530

    move 4a_SwingBasics to HelloSwing.java

--- 4a_SwingBasics/HelloSwing.java
+++ 4a_SwingBasics/HelloSwing.java
@@ -1,8 +1,10 @@
+/* 4a. Basic hello program of Swing displaying the message Hello! VI C , Welcome to Swing
+Programming in Blue color plain font with font size of 32 using Jframe and Jlabel*/
 package program4A;
 import javax.swing.*;
 import java.awt.*;
 
-public class program4A {
+public class HelloSwing{
 	
 	    public static void main(String[] args) {
 	        JFrame frame = new JFrame("Hello Swing");
