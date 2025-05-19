76bde654673ad1153abf4d621b9776c7dea502f8
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-19 20:48:48 +0530

    move 3c_StringManipulation to NumericStringChecker.java

--- 3c_StringManipulation/NumericStringChecker.java
+++ 3c_StringManipulation/NumericStringChecker.java
@@ -1,6 +1,8 @@
+/*Q8. Write a Java Program for Verifying if a string contains only numeric characters using user defined
+function isNumeric() */
 package program3C8;
 
-public class program3C8 {
+public class NumericStringChecker{
 	
 	    public static boolean isNumeric(String str) {
 	        if (str == null || str.isEmpty()) return false;
