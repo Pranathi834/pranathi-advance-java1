f44478ef8d022c50cd71d59c3116140f2dd69cd0
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-19 21:09:33 +0530

    move 5a_SwingJListDemo to CountryListSelection.java

--- 5a_SwingJListDemo/CountryListSelection.java
+++ 5a_SwingJListDemo/CountryListSelection.java
@@ -1,10 +1,14 @@
+/*5a. Develop a Swing program in Java to add the countries USA, India, Vietnam, Canada,
+Denmark, France, Great Britain, Japan, Africa, Greenland, Singapore into a JList and
+display them on console whenever the countries are selected on the list. */
 package program5A;
 
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.event.*;
 
-public class program5A {
+public class CountryListSelection
+ {
 
     // Static inner class that extends JFrame
     static class CountryListDisplay extends JFrame {
