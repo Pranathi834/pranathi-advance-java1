8fade6b5e87cca0f7c38d2e8cff26d197c5b194f
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-19 20:11:00 +0530

    move 3a_AdvancedStringOperations to AdvancedStringDemo.java

--- 3a_AdvancedStringOperations/AdvancedStringDemo.java
+++ 3a_AdvancedStringOperations/AdvancedStringDemo.java
@@ -1,6 +1,12 @@
+*/3a. Develop a java program for performing various string operations with different string
+handling functions directed as follows:
+String Creation and Basic Operations, Length and Character Access, String Comparison, String
+Searching, Substring Operations , String Modification, Whitespace Handling, String Concatenation,
+String Splitting, StringBuilder Demo, String Formatting , Validate Email with contains( ) and
+startsWith() and endsWith()*/
 package program3A;
 import java.util.Scanner;
-public class program3A {
+public class AdvancedStringDemo {
 	
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
