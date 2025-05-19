de02b508e137179434a80d33fc49b3f512c8692a
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-19 20:00:01 +0530

    move StringOperationsDemo.java to 2a_StringOperations

--- 2a_StringOperations/StringOperationsDemo.java
+++ 2a_StringOperations/StringOperationsDemo.java
@@ -1,6 +1,12 @@
+/*2 a. Develop a java program for performing various string operations with different string
+handling functions directed as follows
+String Creation and Basic Operations, Length and Character Access, String Comparison, String
+Searching, Substring Operations , String Modification, Whitespace Handling, String
+Concatenation, String Splitting, StringBuilder Demo, String Formatting , Validate Email with
+contains( ) and startsWith() and endsWith()*/
 package program2A;
 import java.util.Scanner;
-public class program2A {
+public class StringOperationsDemo {
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 
