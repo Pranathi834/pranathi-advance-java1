189915c2182a9736ac62a21898ce8d592776fba9
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-19 19:38:13 +0530

    Update LinkedListIterationFromSecond.java

--- I1c. Linked List Programs/LinkedListIterationFromSecond.java
+++ I1c. Linked List Programs/LinkedListIterationFromSecond.java
@@ -1,3 +1,5 @@
+/*Write a Java program to iterate through all elements in a linked list starting at the
+specified position ( 2nd) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))*/
 package program1C1;
 import java.util.Iterator;
 import java.util.LinkedList;
