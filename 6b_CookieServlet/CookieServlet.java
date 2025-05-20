1e537277686c11f9d4a8be46acdf15ae9c2a1064
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-20 18:33:27 +0530

    Update CookieServlet.java

--- 6b_CookieServlet/CookieServlet.java
+++ 6b_CookieServlet/CookieServlet.java
@@ -1,4 +1,6 @@
-
+/* 6b. Build a servlet program to  create a cookie to get your name through text box and press submit
+ button( through HTML)  to display the message by greeting Welcome back your name ! , you have
+ visited this page n times ( n = number of your visit ) and demonstrate the expiry of cookie also. */
 
 import java.io.IOException;
 import java.io.PrintWriter;
