b1f82ee876acfc63bc0b8f2f7654fc60b8130bda
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-21 15:09:49 +0530

    move 1a_linkedlistprograms to ArrayListlinkedlist.java

--- 1a_linkedlistprograms/ArrayListlinkedlist.java
+++ 1a_linkedlistprograms/ArrayListlinkedlist.java
@@ -1,7 +1,10 @@
+/*1.a Develop a Java program for adding elements [Apple, Banana, Orange] into an ArrayList and a LinkedList to perform the following operations with different functions directed as follows 
+1. Adding elements, 2. Adding element at specific index, 3. Adding multiple elements, 4.  Accessing elements,5. Updating elements, 6. Removing elements, 7. Searching elements, 8. List  size, 9. Iterating over list, 10. Using Iterator, 11. Sorting, 12. Sublist, 13. Clearing the list 
+*/
 package programs1a;
 import java.util.*;
 
-public class program1A {
+public class ArrayListlinkedlist {
 	    public static void main(String[] args) {
 	        // Initialize ArrayList and LinkedList
 	        List<String> arrayList = new ArrayList<>();
