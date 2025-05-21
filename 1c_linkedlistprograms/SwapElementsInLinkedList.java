e82f016606b67156fa2f49f6410666cdb829b708
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-21 19:12:44 +0530

    Update and rename program1C5.java to SwapElementsInLinkedList.java

--- 1c_linkedlistprograms/SwapElementsInLinkedList.java
+++ 1c_linkedlistprograms/SwapElementsInLinkedList.java
@@ -1,7 +1,9 @@
+/*Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
+Collections.swap(l_list, 0, 2))*/
 package program1C5;
 import java.util.LinkedList;
 import java.util.Collections;
-public class program1C5 {
+public class SwapElementsInLinkedList {
 
 	    public static void main(String[] args) {
 	        // Create a LinkedList of Strings
