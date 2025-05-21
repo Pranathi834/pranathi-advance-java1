54404ea974cbc7032aa6faf537d5173322a37561
Pranathi834 <bhatpranathialangar@gmail.com>
2025-05-21 19:24:30 +0530

    move 6c_PrimeNumberServlet to PrimeNumberServlet.java

--- 6c_PrimeNumberServlet/PrimeNumberServlet.java
+++ 6c_PrimeNumberServlet/PrimeNumberServlet.java
@@ -1,4 +1,5 @@
-
+/*6c. Build a servlet program to check the given number is prime number or not using HTML with step
+by step procedure.*/
 
 import java.io.IOException;
 import java.io.PrintWriter;
